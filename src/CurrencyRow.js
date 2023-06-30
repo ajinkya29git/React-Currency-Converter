d7acef250be6ee91817a1c7a30866af25b303994
ajinkya29git <jadhavajinkya29git@gmail.com>
2023-06-30 19:27:48 +0530

    Plain template without any data and functionality

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,21 +1,20 @@
 import React from 'react'
 
 export default function CurrencyRow(props) {
-  const {
-    currencyOptions,
-    selectedCurrency,
-    onChangeCurrency,
-    onChangeAmount,
-    amount
-  } = props
+  // const {
+  //   currencyOptions,
+  //   selectedCurrency,
+  //   onChangeCurrency,
+  //   onChangeAmount,
+  //   amount
+  // } = props
   return (
     <div>
-      <input type="number" className="input" value={amount} onChange={onChangeAmount} />
-      <select value={selectedCurrency} onChange={onChangeCurrency}>
-        {currencyOptions.map(option => (
-          <option key={option} value={option}>{option}</option>
-        ))}
+      <input type="number" className="input" />
+      <select>
+        <option value="Hi">Hi</option>
       </select>
+      
     </div>
   )
 }
